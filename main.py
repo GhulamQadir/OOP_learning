f85e43e91b955813ea50928e8bbd624d381c6314
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2025-08-19 20:30:42 +0500

    n-dimensional vector class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from vector.vector import Vector2d
+from vector.vector import Vector
 from point.point import Point
 from rational_number.rational_number import RationalNumber
 from random_utilities.random_utilities import RandomUtilities
@@ -52,40 +52,85 @@ def main():
     # print(rational6)
 
     ################## Random Utilities Class ###############
-    r1 = RandomUtilities()
+    # r1 = RandomUtilities()
 
-    prog_languages = [
-        "Java",
-        "Python",
-        "C",
-        "C#",
-        "Kotlin",
-        "Rust",
-        "R",
-    ]
+    # prog_languages = [
+    #     "Java",
+    #     "Python",
+    #     "C",
+    #     "C#",
+    #     "Kotlin",
+    #     "Rust",
+    #     "R",
+    # ]
 
-    #### Shuffle Example ####
-    shuffled_list1 = r1.shuffle(prog_languages)
-    print(shuffled_list1)
+    # #### Shuffle Example ####
+    # shuffled_list1 = r1.shuffle(prog_languages)
+    # print(shuffled_list1)
 
     # shuffled_list1 = r1.shuffle("python") # TypeError
     # print(shuffled_list1)
 
-    #### Choice Example  ####
-    random_element1 = r1.choice(range(10))  # range type
-    print(random_element1)
+    # #### Choice Example  ####
+    # random_element1 = r1.choice(range(10))  # range type
+    # print(random_element1)
 
-    random_element2 = r1.choice(prog_languages)  # list type
-    print(random_element2)
+    # random_element2 = r1.choice(prog_languages)  # list type
+    # print(random_element2)
 
-    random_element3 = r1.choice(("Object-Oriented"))  # string type
-    print(random_element3)
+    # random_element3 = r1.choice(("Object-Oriented"))  # string type
+    # print(random_element3)
 
-    random_element4 = r1.choice(("Springboot", "Java"))  # tuple type
-    print(random_element4)
+    # random_element4 = r1.choice(("Springboot", "Java"))  # tuple type
+    # print(random_element4)
 
     # random_element4 = r1.choice({"name":"abc"}) # TypeError
     # print(random_element4)
+
+    ############## VECTOR ####################
+
+    # Create a vector v1 with 3 dimensions
+    v1 = Vector(3)
+
+    # Set elements of v1
+    v1[0] = 1  # Set first element to 1
+    v1[1] = 2  # Set second element to 2
+    v1[2] = 3  # Set third element to 3
+
+    # Create another vector v2 with 3 dimensions
+    v2 = Vector(3)
+
+    # Set elements of v2
+    v2[0] = 2  # Set first element to 2
+    v2[1] = 4  # Set second element to 4
+    v2[2] = 1  # Set third element to 1
+
+    # Calculate and print magnitude (length) of vector v1
+    print("Magnitude-->", v1.magnitude())
+
+    # Calculate and print distance between v2 and v1
+    print("Distance-->", v2.distance(v1))
+
+    # Calculate and print dot product of v1 and v2
+    print("DotProduct-->", v1.dot_product(v2))
+
+    # Calculate and print addition of vectors v2 + v1
+    print("Addition-->", v2 + v1)
+
+    # Calculate and print subtraction of vectors v1 - v2
+    print("Subtraction-->", v1 - v2)
+
+    # Access and print the second element of v1
+    print("Access element-->", v1[1])
+
+    # Update second element of v2
+    v2[1] = 8
+
+    # Check and print the updated second element of v2
+    print("Check Updated element-->", v2[1])
+
+    # Print the length (number of dimensions) of v2
+    print("Length of vector", len(v2))
 
 
 if __name__ == "__main__":
